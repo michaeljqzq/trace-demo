e2fd6b77b1c8bb513b61ea49896eca1565007011
michaeljqzq <michaeljqzq@gmail.com>
2018-10-18 14:14:12 +0800

    Fix package.json for web app deployments

---
+++ api/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, '../build')
+  },
+  target: 'node',
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'web.config', to: '.' }
+    ])
+  ]
+};
