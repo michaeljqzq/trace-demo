5cd13db056b77bba43eaa0ca7bdde7699fb3853a
michaeljqzq <michaeljqzq@gmail.com>
2018-10-18 14:13:44 +0800

    Remove fake

--- api/index.js
+++ api/index.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 // const cors = require('cors');
 
 const data = require('./data');
-const fake = require('./fake');
+// const fake = require('./fake');
 const backend = require('./backend');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/api/data', data);
-app.use('/api/fake', fake);
+// app.use('/api/fake', fake);
 app.use('/api/backend', backend);
 
 app.use(express.static('.'));
