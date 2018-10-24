341f4073a3ab4c30fd0b49926c5628c54fc19c27
michaeljqzq <michaeljqzq@gmail.com>
2018-10-24 20:37:23 +0800

    Deploy script

---
+++ one-click-cn.ps1
@@ -0,0 +1,8 @@
+npm run build-all
+cd build
+git init
+git a
+git c "1"
+git remote add up https://zhqqi2@techsummitedgedemoportalcn.scm.chinacloudsites.cn:443/techsummitedgedemoportalcn.git
+git push up master -f
+cd ..
---
+++ one-click.ps1
@@ -0,0 +1,8 @@
+npm run build-all
+cd build
+git init
+git a
+git c "1"
+git remote add up https://zhqqi2@techsummitedgedemoportal.scm.azurewebsites.net:443/techsummitedgedemoportal.git
+git push up master -f
+cd ..
